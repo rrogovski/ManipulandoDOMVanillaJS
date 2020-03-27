ae5e7b0c9ea039e5c8e8d6bf8fedf5cb97117469
Rodrigo Rogovski <rfrogovski@gmail.com>
2020-03-27 16:18:51 -0400

    html+css+js

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+const inputElement = document.querySelector("input");
+const buttonElement = document.querySelector("form button");
+const ulElement = document.querySelector("ul");
+
+buttonElement.onclick = ev => {
+  ev.preventDefault();
+
+  if (inputElement.value) {
+
+    const textElement = document.createElement("span");
+    textElement.innerHTML = inputElement.value;
+
+    const btnElement = document.createElement("button");
+    btnElement.innerHTML = '<i class="material-icons">delete_outline</i>';
+
+    const liElement = document.createElement("li");
+    liElement.appendChild(textElement);
+    liElement.appendChild(btnElement);
+    
+
+    btnElement.onclick = () => {
+      ulElement.removeChild(liElement)  
+    }
+
+    ulElement.appendChild(liElement);
+    inputElement.value = "";
+  }
+};
